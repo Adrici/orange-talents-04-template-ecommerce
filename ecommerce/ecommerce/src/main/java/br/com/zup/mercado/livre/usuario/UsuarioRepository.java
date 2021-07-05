c8512e6abd1648bb03a102e69d27bb5cd40b5a79
Adrici <cirellia5@gmail.com>
2021-07-05 11:01:35 -0300

    0-0-0-setup.md

--- ecommerce/ecommerce/src/main/java/br/com/zup/mercado/livre/usuario/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.zup.mercado.livre.usuario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
-
-}
